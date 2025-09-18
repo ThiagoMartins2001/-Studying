17cc2db69f79e71fca9a894c5ff647c4fdc5857c
Thiago Martins <158003020+ThiagoMartins2001@users.noreply.github.com>
2025-09-18 18:04:49 -0300

    identificador de impa

---
+++ identificadorDeImpa/identificadorDeImpa.java
@@ -0,0 +1,20 @@
+package identificadorDeImpa;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class identificadorDeImpa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
